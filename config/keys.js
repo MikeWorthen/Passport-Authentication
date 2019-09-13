0b919a0a1b2857103c5277647df5410da1a7df80
Mike Worthen <mikeworthen55@gmail.com>
2019-09-13 13:06:17 -0600

    using MongoCompass now

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://mworth55:WORTHsilver55@m-mcluster-o9z5m.mongodb.net/test?retryWrites=true&w=majority'
+    // MongoURI: 'mongodb+srv://mworth55:WORTHsilver55@m-mcluster-o9z5m.mongodb.net/test?retryWrites=true&w=majority'
+
+    MongoURI: 'mongodb+srv://mworth55:WORTHsilver55@m-mcluster-o9z5m.mongodb.net/test'
 }
